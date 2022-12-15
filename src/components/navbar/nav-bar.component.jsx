c73fcea001078ac774f487aaefc3a304e5427b22
Janice Shih <tut@howard.edu>
2022-12-14 21:15:41 -0500

    Add webpack.config.js

--- src/components/navbar/nav-bar.component.jsx
+++ src/components/navbar/nav-bar.component.jsx
@@ -5,8 +5,7 @@ const NavBar = () => {
   return (
     <>
       <div className={classes.site__top_hat}>
-        navbar
-        {/* <div className="nav-secondary"></div> */}
+        <div className={classes.navSecondary}></div>
       </div>
     </>
   );
