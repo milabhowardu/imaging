cd04bb30be82d915a9a43681eb0cfb43a0f4d3f8
Janice Shih <tut@howard.edu>
2022-12-14 23:05:08 -0500

    lab title and favicon.ico

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Home from './components/home/home.component';
-// import Test from './components/test/test.component';
-// import NotFound from './components/not-found/not-found.component';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/home/home.component';
+import Test from './components/test/test.component';
+import NotFound from './components/not-found/not-found.component';
 import NavBar from './components/navbar/nav-bar.component';
+import Hero from './components/hero/hero.component';
 
 function App() {
   return (
     <> 
     <NavBar />
-    Department of Radiology
-    {/* hero
+    <Hero />
     <Router>
       <Routes>
           <Route exact path="/" element={<Home />}/>
@@ -18,7 +18,8 @@ function App() {
           <Route path="*" element={<NotFound />}/>
       </Routes>
     </Router>
-    footer */}
+    Department of Radiology
+    footer
   </>
   );
 }
