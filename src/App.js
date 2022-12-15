b6d24a87d939cc5b04d609af792b118b515e2763
Janice Shih <tut@howard.edu>
2022-12-15 01:53:48 -0500

    home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/home/home.component';
-import Test from './components/test/test.component';
-import NotFound from './components/not-found/not-found.component';
-import NavBar from './components/navbar/nav-bar.component';
-import Hero from './components/hero/hero.component';
-import Footer from './components/footer/footer.component';
+import Home from './components/page-components/home/home.component';
+import Test from './components/page-components/test/test.component';
+import NotFound from './components/page-components/not-found/not-found.component';
+
+import NavBar from './components/base-components/navbar/nav-bar.component';
+import Hero from './components/base-components/hero/hero.component';
+import Footer from './components/base-components/footer/footer.component';
 
 function App() {
   return (
---
+++ src/components/page-components/not-found/not-found.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFound = () => {
+  return <div style={{ height: 200 }}>not found</div>;
+};
+
+export default NotFound;
