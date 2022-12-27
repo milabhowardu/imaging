f198e6bbb08c20b3878d66c0360bbd2fc183c47d
Janice Shih <tut@howard.edu>
2022-12-27 17:03:24 -0500

    Add facilities page hero and nav path

--- src/components/page-components/facilities/facilitiescomponent.jsx
+++ src/components/page-components/facilities/facilitiescomponent.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import classes from "./facilities.module.sass";
+import Hero from "../../base-components/hero/hero.component";
+
+import { ImLab } from "react-icons/im";
+
+import background from "../../../assets/images/Facilities.jpg";
 
 const Facilities = () => {
   return (
     <>
-      <div className={classes.wrapper}>Facilities</div>
+      <Hero background={background} title="" />
+      <div className={classes.wrapper}>
+        <div className={classes.navPath}>
+          HOME » <strong>Facilites</strong>
+        </div>
+        <div className={classes.blockHeader}>
+          <div>
+            <ImLab size={25} /> Facilities: Biomedical Imaging
+          </div>
+        </div>
+
+
+      </div>
     </>
   );
 };
