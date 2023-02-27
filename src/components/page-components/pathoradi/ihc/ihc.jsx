75fda9319b3045e095240266f48a75d9ce155a28
Janice Shih <tut@howard.edu>
2023-02-26 20:11:30 -0500

    instal d3.js

--- src/components/page-components/pathoradi/ihc/ihc.jsx
+++ src/components/page-components/pathoradi/ihc/ihc.jsx
@@ -6,22 +6,24 @@ import { FaBrain } from "react-icons/fa";
 import background from "../../../../assets/images/Facilities.jpg";
 import SearchBox from "../searchBox/search-box.component";
 
-import { select } from "d3";
+// import { select } from "d3";
 
 const data = [25, 30, 45, 60, 20];
 
 const Ihc = () => {
   const svgRef = useRef();
   
-  useEffect(()=>{
-    console.log(svgRef);
-    const svg = select(svgRef.current);
-    // svg.selectAll(("circle")
-    //     .data(data)
-    //     .join(
-
-    //     ));
-  }, []);
+//   useEffect(()=>{
+//     console.log(svgRef);
+//     const svg = select(svgRef.current);
+//     svg.selectAll(("circle")
+//         .data(data)
+//         .join(
+//             enter => enter.append("circle"),
+//             update => update.att("class", "updated"),
+//             exit => exit.remove()
+//         ));
+//   }, []);
 
   return (
     <>
