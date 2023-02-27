8a8edfbcca46998e8e089caa4257ddd33bf49aa9
Janice Shih <tut@howard.edu>
2023-02-26 20:05:01 -0500

    install d3.js

--- src/components/page-components/pathoradi/ihc/ihc.jsx
+++ src/components/page-components/pathoradi/ihc/ihc.jsx
@@ -1,31 +1,42 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import classes from "./ihc.module.sass";
+
 import Hero from "../../../base-components/hero/hero.component";
-
-import { ImLab } from "react-icons/im";
-
+import { FaBrain } from "react-icons/fa";
 import background from "../../../../assets/images/Facilities.jpg";
 import SearchBox from "../searchBox/search-box.component";
 
+import { select } from "d3";
+
+const data = [25, 30, 45, 60, 20];
+
 const Ihc = () => {
+  const svgRef = useRef();
+  
+  useEffect(()=>{
+    console.log(svgRef);
+    const svg = select(svgRef.current);
+    // svg.selectAll(("circle")
+    //     .data(data)
+    //     .join(
+
+    //     ));
+  }, []);
+
   return (
     <>
-      <Hero
-        title="Visualization for IHC/MRI"
-        subtitle=" Upload raw image to get result. "
-        register="Register"
-      />
       <div className={classes.wrapper}>
         <div className={classes.navPath}>
-          HOME » PathRadi » <strong>IHC</strong>
+          HOME » <a href="/pathoradi">PathRadi</a> » <strong>IHC</strong>
         </div>
         {/* <SearchBox /> */}
-
-        {/* <div className={classes.blockHeader}>
+        <div className={classes.blockHeader}>
           <div>
-            <ImLab size={25} /> IHC
+            <FaBrain size={25} /> IHC
           </div>
-        </div> */}
+        </div>
+
+        <svg className={classes.ihcCanvas} ref={svgRef}></svg>
 
       </div>
     </>
