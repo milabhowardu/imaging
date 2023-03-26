dcb0601b14cffa9b0c161cbb8c2b31690f75c746
Janice Shih <tut@howard.edu>
2023-03-26 18:13:51 -0400

    Upload Styling

--- src/components/page-components/pathoradi/upload/file-upload/file-upload.component.jsx
+++ src/components/page-components/pathoradi/upload/file-upload/file-upload.component.jsx
@@ -20,14 +20,49 @@ const FileUpload = ({
   const fileInputField = useRef(null);
   const [files, setFiles] = useState({});
 
+  const handleUploadBtnClick = () => {
+    fileInputField.current.click();
+  };
+
+  const addNewFiles = (newFiles) => {
+    for (let file of newFiles) {
+      if (file.size <= maxFileSizeInBytes) {
+        if (!otherProps.multiple) {
+          return { file };
+        }
+        files[file.name] = file;
+      }
+    }
+    return { ...files };
+  };
+
+  const callUpdateFilesCb = (files) => {
+    const filesAsArray = convertNestedObjectToArray(files);
+    updateFilesCb(filesAsArray);
+  };
+
+  const handleNewFileUpload = (e) => {
+    const { files: newFiles } = e.target;
+    if (newFiles.length) {
+      let updatedFiles = addNewFiles(newFiles);
+      setFiles(updatedFiles);
+      callUpdateFilesCb(updatedFiles);
+    }
+  };
+
+  const removeFile = (fileName) => {
+    delete files[fileName];
+    setFiles({ ...files });
+    callUpdateFilesCb({ ...files });
+  };
+
   return (
     <>
       <label>{label}</label>
       <div className={classes.wrapper}>
         <section>
           <p>Drag and drop your files anywhere or</p>
-          <button type="button">
-            {/* <FaCloudUploadAlt size={25} />  */}
+          <button type="button" onClick={handleUploadBtnClick}>
             <span>  Upload {otherProps.multiple ? "files" : "a file"}</span>
           </button>
           <input
