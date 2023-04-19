c8025eee95749ccafaa2691c93827da6081aa93f
Janice Shih <tut@howard.edu>
2023-04-18 23:06:16 -0400

    setup as private

--- src/components/page-components/people/person/sunny.component.jsx
+++ src/components/page-components/people/person/sunny.component.jsx
@@ -12,7 +12,7 @@ const Sunny = () => {
     biography: `Sunny Ji is a research assistant at the Molecular Imaging Laboratory in the Department of Radiology at Howard University. Since graduating from Virginia Tech with a Bachelor of Science in Biology, and a concentration in immunology and microbiology, Sunny has focused on clinical medicine and research. She is currently collaborating with Children’s National Hospital in utilizing Magnetic Resonance Imaging (MRI) to study the effects of anoxia on neonatal rat brains. Her work also involves integrating machine learning with histology to capture microglial cell morphology and segmentation. Additionally, she is working on a project to create a morphological atlas of microglial cells in the rat brain.`,
     areasofExpertise: ["Neuroimaging", "Neuropathology", "Machine Learning"],
     publicationsList:
-      `In progress!`,
+      `In progress! `,
   };
 
   return (
