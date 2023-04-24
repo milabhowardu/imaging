c7aa2ad0d47a9f8612adc5479c925ad59072e69a
Janice Shih <tut@howard.edu>
2023-04-24 18:35:15 -0400

    mandy personal page

--- src/components/page-components/people/person/mandy.component.jsx
+++ src/components/page-components/people/person/mandy.component.jsx
@@ -11,18 +11,45 @@ const Mandy = () => {
     title: "Postdoctoral Fellow",
     phone: "(202) 865-3760",
     email: "beming.chang@howard.edu",
-    biography: ``,
-    areasofExpertise: [],
-    publicationsList:
-      `
-      `,
+    biography: `
+    Dr. Be-Ming Chang is a Post-doctoral fellow in the Molecular Imaging Laboratory of the Department of Radiology at Howard University. She holds a Ph.D. in Chemistry from the National Tsing-Hua University and a Ph.D. in Molecular Science and Technology from the Institute of Atomic and Molecular Sciences at Academia Sinica, Taiwan.
+    <br /><br />
+    Dr. Chang has developed diverse hybrid materials that not only enable integrations of materials with multi-functionality but also expand their utilization in advanced applications. Her research combines expertise in chemistry, chemical structure design, nanotechnology, and polymer for biomedical, optical, electrical, and physical fields. She has comprehensive experience working collaboratively with interdisciplinary research teams, companies, and hospitals worldwide to develop essential products for scientific problem-solving and commercialization. Her research focuses on addressing materials for a wide range of techniques that can overcome key limitations and promote widespread adoption.
+    <br /><br />
+    Dr. Chang received awards, including the Excellent Doctoral Dissertation Award from BASF Corporation, the Outstanding Graduate Student Award from National Taiwan Normal University, Taiwan, and selection honored as a doctoral student to 2014 Nobel Laureates Meeting in Japan.
+
+    `,
+    areasofExpertise: [
+        "Material Development and Analysis",
+        "Imaging technology (Biomedical imaging and Optical physics)",
+        "Multi-disciplinary research"
+    ],
+    publicationsList: `
+    B.-M. Chang, H.-C. Chang, and Dean Ho. “Biomarkers and drug delivery applications.” in Nanodiamonds: Advanced Material Analysis, Properties and Applications. (Elsevier, 2017), Chapter 16.
+    <br /><br />
+    Y-Y Liu, B.-M. Chang, H.-C. Chang, “Nanodiamond-Enabled Biomedical Imaging” Nanomedicine, 2020, 15, 16.
+    <br /><br />
+    B.-M. Chang, L. Pan, H.-H. Lin, H.-C. Chang, ”Nanodiamond-Supported Silver Nanoparticles as Potent and Safe antibacterial agents” Scientific Reports. 2019, 9, 13164.
+    <br /><br />
+    Y.-Y. Hui, Oliver Y. Chen, T. Azuma, B.-M. Chang, F.-J. Hsieh, and H.-C. Chang “All-Optical Thermometry with Nitrogen-Vacancy Centers in Nanodiamond-Embedded Polymer Films.” J. Phys. Chem. C. 2019, 123, 15366-15374.
+    <br /><br />
+    L.-J. Su, M.-S. Wu, Y.-Y. Hui, B.-M. Chang, L.-Pan, P.-J. Hsu, Y.-T. Chen, H.-N. Ho, Y.-H. Huang, T.-Y. Ling, H.-H. Hsu, and H.-C. Chang, “Fluorescent nanodiamonds enables quantitative tracking of human mesenchymal stem cells in miniature pigs.” Scientific Reports. 2017, 7, 45607.
+    <br /><br />
+    B.-M. Chang, C.-H. Hsu, Y.-Y. Hui, C.-C. Wu, T.-Y. Yu, Y.-Y. Lin, and H.-C. Chang, “Multiscale Imaging of Tumor Architecture by Optical Nanodiamonds and Magnetic Nanoparticles” (Submitted).
+    <br /><br />
+    B.-M. Chang, H.-H. Lin, L.-J. Su, W.-D. Lin, R.-J. Lin, Y.-K. Tzeng, Reiko T. Lee, Yuan C. Lee, Alice L. Yu and H.-C. Chang. “Highly Fluorescent Nanodiamonds Protein-Functionalized for Cell labeling and Targeting.” Adv. Funct. Mater. 2013, 23, 5737 -5745.
+    <br /><br />
+    Y.-K. Tzeng, Orestis Falkaris, B.-M. Chang, Y. Kuo, J.-H. Hsu, and H.-C. Chang, “Superresolution Imaging of Albumin-Conjugated Fluorescent Nanodiamonds in Cells by Stimulated Emission Depletion.” Angew. Chem. Int. Ed., 2011, 50, 2262-2265.
+    <br /><br />
+    Y.-K. Tzeng, Orestis Falkaris, Be-Ming Chang, Y. Kuo, J.-H. Hsu, and H.-C. Chang, “Superresolution Imaging of Albumin-Conjugated Fluorescent Nanodiamonds in Cells by Stimulated Emission Depletion.” Angew. Chem., 2011, 123, 2310-2013.
+
+    `,
   };
   return (
     <>
-      <Person person={person}/>
+      <Person person={person} />
     </>
   );
-  
 };
 
 export default Mandy;
